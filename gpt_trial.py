d2e9f56178a997ba3675792df8ba097d927e78b3
Ziyi Wang <zw67@illinois.edu>
2023-02-25 15:42:20 -0600

    basic functions

--- gpt_trial.py
+++ gpt_trial.py
@@ -1,7 +1,8 @@
 import os
 import openai
 
-openai.api_key = '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'  # API key
+openai.api_key = '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'  # API key
+
 
 def response(promp):
     response = openai.Completion.create(
@@ -18,7 +19,22 @@ def response(promp):
     print(result)
     return
 
-def bestOfWeek(conversation):
-    
 
-response("what day is it today?")
+def bestOfWeek(conversation):
+    conversation = "please select the funniest quote of the week: \n" + conversation
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=conversation,
+        temperature=0,
+        max_tokens=64,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=[";"]
+    )
+    result = response.choices[0].text
+    print(result)
+
+
+podcast = open("./testcase.txt", "r").read()
+bestOfWeek(podcast)
